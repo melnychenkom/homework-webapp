0a8afc8a886fb4c3eb113cdb6d32e57ed49fde79
MelnychenkoM <miko.melnychenko@gmail.com>
2026-04-03 11:09:14 +0300

    feat: add all views, templates, and tests (messages, index, submit, delete)

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('messages/', views.messages, name='messages'),
+    path('submit/', views.submit, name='submit'),
+    path('delete/', views.delete_message, name='delete_message'),
+]

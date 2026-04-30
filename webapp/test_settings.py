920432252fa213951100e0ffb62d0541f58bcb5c
MelnychenkoM <miko.melnychenko@gmail.com>
2026-04-30 22:36:39 +0300

    chore: remove unused test_settings.py

--- webapp/test_settings.py
+++
@@ -1,5 +0,0 @@
-"""Test-only settings overrides."""
-from .settings import *  # noqa: F401, F403
-
-# Django 4.0 requires CSRF_TRUSTED_ORIGINS values to include a scheme.
-CSRF_TRUSTED_ORIGINS = ["https://group3.gcs-camp-conference.org"]
